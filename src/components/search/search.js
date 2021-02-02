b83fd7b0d0fcaef3d4b8c2576c39fe35cd869864
TheNeelOfficial <theneelofficial@gmail.com>
2021-02-02 20:41:23 +1100

    added algolia places module for search component

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,17 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import AlgoliaPlaces from 'algolia-places-react';
+import { useHistory } from "react-router-dom";
 
 
 
-class Search extends Component {
+const Search = ({ search }) => {
 
-  render() {
-    
+const history = useHistory();
+
+const [searchLocation, setSearchLocation] = React.useState('Melbourne, Victoria, Australia');    
+
       return (
         <div className="Search_bar">
-          <p>Search Bar</p>
+                <AlgoliaPlaces
+                    placeholder='Enter location'
+
+                    options={{
+                        language: 'en',
+                    }}
+                    // send search input as a url param
+                    onChange={e => {
+                        setSearchLocation(e.suggestion.value)
+                        history.push("/search/" + e.suggestion.value);
+                    }}
+                />
         </div>
       )
-  }
 }
 
 
